002225975c57ee61fd10d3f16fcbdbe71155cd4f
LudvinUmes <134966081+LudvinUmes@users.noreply.github.com>
2024-04-25 01:49:47 -0600

    Proyecto en consola, con funcionalidad de transmitir Json a Arduino

---
+++ Gasolinera/Bomba.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gasolinera
+{
+    internal class Bomba
+    {
+        public string Codigo { get; set; }
+        public int LitrosDisponibles { get; set; }
+
+        public Bomba(string codigo, int litrosDisponibles)
+        {
+            Codigo = codigo;
+            LitrosDisponibles = litrosDisponibles;
+        }
+
+    }
+}
